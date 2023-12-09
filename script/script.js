6a85d7afcc342946d19f5de2608b53a4b7eb221c
Nighthawks829 <liew_shai_bi21@iluv.ums.edu.my>
2023-12-09 10:03:41 +0800

    front end

---
+++ script/script.js
@@ -0,0 +1,9 @@
+// setInterval(function(){location.reload(true);}, 5000);
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
